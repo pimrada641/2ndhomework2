aeedb264cc0be5a8b27782f036141d67f5ab7760
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2021-10-09 22:56:35 +0700

    Update ClassPersonList.cs

--- ClassPersonList.cs
+++ ClassPersonList.cs
@@ -3,23 +3,23 @@ using System;
 
 class PersonList
 {
-	private List<Person> personlist;
-	public PersonList()
+	private List<Person> personlist; //สร้างลิสต์ที่เป็นข้อมูลส่วนตัว
+	public PersonList() //Constructor ลิสต์รายชื่อ
     {
         this.personlist = new List<Person>();
     }
-    public void AddNewPerson(Person person)
+    public void AddNewPerson(Person person) //ฟังก์ชั่นเพิ่มรายชื่อ
     {
         this.personlist.Add(person);
     }
 
-    public void RemovePerson()
+    public void RemovePerson() //เมธอดลบรายชื่อในลิสต์ตามที่ผู้ใช้กรอก
     {
         int i = int.Parse(Console.ReadLine());
         this.personlist.RemoveAt(i-1);
     }
 
-    public void FetchPersonList()
+    public void FetchPersonList() //เมธอดแสดงรายชื่อทั้งหมดจากในลิสต์
     {
         foreach(Person person in this.personlist)
         {
