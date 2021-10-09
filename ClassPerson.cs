ad105a836252828358891383009e98b6f07edb3f
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2021-10-09 23:03:02 +0700

    Update ClassPerson.cs

--- ClassPerson.cs
+++ ClassPerson.cs
@@ -1,35 +1,35 @@
 ﻿public class Person
 {
-    public string name;
-    public string address;
-    public string citizenID;
+    public string name; //กำหนดตัวแปรชื่อ
+    public string address; //กำหนดตัวแปรที่อยู่
+    public string citizenID; //กำหนดตัวแปรเลขบัตรประชาชน
 
-    public Person(string name, string address, string citizenID)
+    public Person(string name, string address, string citizenID) //Constructor รับข้อมูลชื่อ ที่อยู่ เลขบัตรประชาชน
     {
         this.name = name;
         this.address = address;
         this.citizenID = citizenID;
     }
 
-    public string Getname()
+    public string Getname() //เมธอดแสดงผลชื่อ
     {
         return this.name;
     }
 }
-class Student : Person
+class Student : Person //Class Student ที่สืบทอดมาจาก class Person
 {
-    private string studentID;
+    private string studentID; //กำหนดตัวแปรรหัสนักเรียนเป็นส่วนตัว
 
-    public Student(string name,string address, string citizenID, string studentID) : base(name, address, citizenID)
+    public Student(string name,string address, string citizenID, string studentID) : base(name, address, citizenID) //Constructor รับค่าชื่อ ที่อยู่ เลขบัตรประชาชนตาม Class Person และค่ารหัสนักเรียนจากในคลาสนี้
     {
-        this.studentID = studentID;
+        this.studentID = studentID; 
     }
 }
-class Teacher : Person
+class Teacher : Person //Class Teacher ที่สืบทอดมาจาก class Person
 {
-    private string employeeID;
+    private string employeeID; //กำหนดตัวแปรรหัสพนักงานเป็นส่วนตัว
 
-    public Teacher(string name, string address, string citizenID, string employeeID) : base(name, address, citizenID)
+    public Teacher(string name, string address, string citizenID, string employeeID) : base(name, address, citizenID) //Constructor รับค่าชื่อ ที่อยู่ เลขบัตรประชาชนตาม Class Person และค่ารหัสพนักงานจากในคลาสนี้
     {
         this.employeeID = employeeID;
     }
