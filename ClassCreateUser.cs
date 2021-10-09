18e4ae3503304b1864d41415abfaf68cf6a701a9
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2021-10-09 23:06:32 +0700

    Update ClassCreateUser.cs

--- ClassCreateUser.cs
+++ ClassCreateUser.cs
@@ -3,39 +3,39 @@ using homework2;
 
 class CreateNewUser
 {
-    public static Student CreateNewStudent()
+    public static Student CreateNewStudent() //ฟังก์ชั่นสร้างรายชื่อนักเรียนใหม่โดยรับและส่งค่าจากฟังก์ชั่นต่างๆ 
     {
         return new Student(InputName(), InputAddress(), InputCitizenID(), StudentID());
     }
-    public static Teacher CreateNewTeacher()
+    public static Teacher CreateNewTeacher() //ฟังก์ชั่นสร้างรายชื่ออาจารย์ใหม่โดยรับและส่งค่าจากฟังก์ชั่นต่างๆ 
     {
         return new Teacher(InputName(), InputAddress(), InputCitizenID(), EmployeeID());
     }
-    static string InputName()
+    static string InputName() //ฟังก์ชั่นรับและส่งค่าชื่อ
     {
         Console.Write("Name: ");
 
         return Console.ReadLine();
     }
-    static string InputAddress()
+    static string InputAddress() //ฟังก์ชั่นรับและส่งค่าที่อยู่
     {
         Console.Write("Address: ");
 
         return Console.ReadLine();
     }
-    static string InputCitizenID()
+    static string InputCitizenID() //ฟังก์ชั่นรับและส่งค่าเลขบัตรประชาชน
     {
         Console.Write("CitizenID: ");
 
         return Console.ReadLine();
     }
-    static string StudentID()
+    static string StudentID() //ฟังก์ชั่นรับและส่งค่ารหัสนักเรียน
     {
         Console.Write("StudentID: ");
 
         return Console.ReadLine();
     }
-    static string EmployeeID()
+    static string EmployeeID() //ฟังก์ชั่นรับและส่งค่ารหัสพนักงาน
     {
         Console.Write("EmployeeID: ");
 
