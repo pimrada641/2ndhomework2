9db15b68c676cc32601c72b239886f2d6d282218
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2021-10-09 23:08:19 +0700

    Update ClassAllScreen.cs

--- ClassAllScreen.cs
+++ ClassAllScreen.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 public class Screen
 {
-    static public void mainmenuscreen()
+    static public void mainmenuscreen() //เมธอดแสดงหน้า main menu
     {
         Console.WriteLine("Welcome to registration new user school application.\n" +
             "-----------------------------------\n" +
@@ -10,23 +10,23 @@ public class Screen
             "3. Get List Persons.\n"+
             "4. Delete person in list.");
     }
-    static public void headtotalstudentscreen()
+    static public void headtotalstudentscreen() //เมธอดแสดงหน้า Register new student
     {
         Console.WriteLine("Register new student.\n" +
             "--------------------");
     }
 
-    static public void headtotalteacherscreen()
+    static public void headtotalteacherscreen() //เมธอดแสดงหน้า Register new teacher
     {
         Console.WriteLine("Register new teacher.\n" +
         "--------------------");
     }
-    static public void personListscreen()
+    static public void personListscreen() //เมธอดแสดงหน้า List Persons
     {
         Console.WriteLine("List Persons\n"+
         "------------");
     }
-    static public void deletepersonscreen()
+    static public void deletepersonscreen() //เมธอดแสดงหน้า Delete person in list
     {
         Console.WriteLine("Delete person in list\n" +
         "---------------");
